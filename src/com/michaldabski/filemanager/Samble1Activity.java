c9cf79e94b177392b484ceb40cd540c4eef999a0
skycloud36 <1076626296@qq.com>
2021-11-28 18:54:07 +0800

    first activity1.0

---
+++ src/com/michaldabski/filemanager/Samble1Activity.java
@@ -0,0 +1,17 @@
+package com.michaldabski.filemanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.View;
+
+public class Samble1Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samble1);
+    }
+}
